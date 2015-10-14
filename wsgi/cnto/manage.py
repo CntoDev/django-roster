393024b85696d1331f046b9ea7d093458f33558e
Riaan Swart <riaan@stonethree.com>
2015-10-14 22:04:42 +0200

    Minor.

--- wsgi/cnto/manage.py
+++ wsgi/cnto/manage.py
@@ -3,8 +3,8 @@ import os
 import sys
 
 if __name__ == "__main__":
-    # GETTING-STARTED: change 'myproject' to your project name:
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
+    # GETTING-STARTED: change 'cnto' to your project name:
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cnto.settings")
 
     from django.core.management import execute_from_command_line
 
