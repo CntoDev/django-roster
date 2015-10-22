08fe3dfafb3c8fc0fe34c35e7b0954c907a73d92
Riaan Swart <riaan@stonethree.com>
2015-10-22 15:59:19 +0200

    Average added to plot.  Fixed #14.

--- wsgi/cnto/cnto/templates/cnto/report/summary.js
+++ wsgi/cnto/cnto/templates/cnto/report/summary.js
@@ -37,22 +37,25 @@ function generateChart(data) {
 //            .attr("y", function(d) { return y(d.week_max); })
 //            .attr("width", x.rangeBand())
 //            .attr("height", function(d) { return height - y(d.week_max); });
-    var margin = {top: 20, right: 30, bottom: 30, left: 40},
-        width = 1200 - margin.left - margin.right,
-        height = 500 - margin.top - margin.bottom;
+    var totalWidth = 1200;
+    var totalHeight = 500;
 
-    var x = d3.scale.ordinal()
+    var margin = {top: 20, right: 30, bottom: 30, left: 40},
+        width = totalWidth - margin.left - margin.right,
+        height = totalHeight - margin.top - margin.bottom;
+
+    var xScale = d3.scale.ordinal()
         .rangeRoundBands([0, width], .1);
 
-    var y = d3.scale.linear()
+    var yScale = d3.scale.linear()
         .range([height, 0]);
 
     var xAxis = d3.svg.axis()
-        .scale(x)
+        .scale(xScale)
         .orient("bottom");
 
     var yAxis = d3.svg.axis()
-        .scale(y)
+        .scale(yScale)
         .orient("left")
         .ticks(20, "");
 
@@ -62,8 +65,15 @@ function generateChart(data) {
       .append("g")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    x.domain(data.map(function(d) { return d.week_start_dt; }));
-    y.domain([0, d3.max(data, function(d) { return d.week_max; })]);
+    xScale.domain(data.map(function(d) { return d.week_start_dt; }));
+    yScale.domain([0, d3.max(data, function(d) { return d.week_max; })]);
+
+    var lineGen = d3.svg.line()
+        .x(function(d) {
+            return xScale(d.week_start_dt) + xScale.rangeBand() / 2;
+        }).y(function(d) {
+            return yScale(d.week_avg);
+        });
 
     chart.append("g")
       .attr("class", "x axis")
@@ -74,15 +84,58 @@ function generateChart(data) {
       .attr("class", "y axis")
       .call(yAxis);
 
-    chart.selectAll(".bar")
-        .data(data)
-    .enter().append("rect")
-        .attr("class", "bar")
-        .attr("x", function(d) { return x(d.week_start_dt); })
-        .attr("y", function(d) { return y(d.week_max); })
-        .attr("height", function(d) { return height - y(d.week_max); })
-        .attr("width", x.rangeBand());
+    var color = d3.scale.ordinal().range(['steelblue', 'limegreen']);
 
+    // Bars
+    var chartEnter = chart.selectAll(".bar").data(data).enter();
+    chartEnter.append("rect")
+        .attr("class", "bar")
+        .attr("x", function(d) { return xScale(d.week_start_dt); })
+        .attr("y", function(d) { return yScale(d.week_max); })
+        .attr("height", function(d) { return height - yScale(d.week_max); })
+        .attr("width", xScale.rangeBand())
+        .attr("fill", function(d, i) {
+            return color("Maximum");
+        });
+
+    // Line
+    chartEnter.append('svg:path')
+        .attr('d', lineGen(data))
+        .attr('stroke', function(d, i) {
+            return color("Average");
+        })
+        .attr('stroke-width', 2)
+        .attr('fill', 'none');
+
+    // Legend
+    var legendRectSize = 18;
+    var legendSpacing = 4;
+
+    var legend = chart.selectAll('.legend')
+            .data(color.domain())
+        .enter()
+            .append('g')
+            .attr('class', 'legend')
+            .attr('transform', function(d, i) {
+                var height = legendRectSize + legendSpacing;
+                var offset =  height * color.domain().length / 2;
+                var horz = -2 * legendRectSize + totalWidth - 100;
+                var vert = i * height - offset;
+                return 'translate(' + horz + ',' + vert + ')';
+            });
+
+    legend.append('rect')
+        .attr('width', legendRectSize)
+        .attr('height', legendRectSize)
+        .style('fill', color)
+        .style('stroke', color);
+
+    legend.append('text')
+        .attr('x', legendRectSize + legendSpacing)
+        .attr('y', legendRectSize - legendSpacing)
+        .text(function(d) { return d; });
+
+    // Add the text label for the X axis
     chart.append("text")
         .attr("transform", "translate(" + (width / 2) + " ," + (height + margin.bottom) + ")")
         .style("text-anchor", "middle")
@@ -95,8 +148,7 @@ function generateChart(data) {
         .attr("x",0 - (height / 2))
         .attr("dy", "1em")
         .style("text-anchor", "middle")
-        .text("Max attendance");
-
+        .text("Attendance");
 }
 
 $(document).ready(function () {
