99f6adc1e5e13ab5a0391a0a04f92294c6621913
Riaan Swart <riaan@stonethree.com>
2015-10-22 21:47:15 +0200

    Minor refactor.

--- wsgi/cnto/cnto/templates/cnto/report/summary.js
+++ wsgi/cnto/cnto/templates/cnto/report/summary.js
@@ -1,42 +1,8 @@
-function generateChart(data) {
-//    var margin = {top: 20, right: 30, bottom: 30, left: 40},
-//        width = 960 - margin.left - margin.right,
-//        height = 500 - margin.top - margin.bottom;
-//
-//    var x = d3.scale.ordinal().rangeRoundBands([0, width], .1);
-//
-//    var y = d3.scale.linear().range([height, 0]);
-//
-//    var xAxis = d3.svg.axis().scale(x).orient("bottom");
-//
-//    var yAxis = d3.svg.axis().scale(y).orient("left");
-//
-//    var chart = d3.select(".chart")
-//        .attr("width", width + margin.left + margin.right)
-//        .attr("height", height + margin.top + margin.bottom)
-//        .append("g")
-//        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-//
-//    x.domain(data.map(function(d) { return d.week_start_dt; }));
-//    y.domain([0, d3.max(data, function(d) { return d.week_max; })]);
-//
-//    chart.append("g")
-//        .attr("class", "x axis")
-//        .attr("transform", "translate(0," + height + ")")
-//        .call(xAxis);
-//
-//    chart.append("g")
-//        .attr("class", "y axis")
-//        .call(yAxis);
-//
-//    chart.selectAll(".bar")
-//            .data(data)
-//        .enter().append("rect")
-//            .attr("class", "bar")
-//            .attr("x", function(d) { return x(d.week_start_dt); })
-//            .attr("y", function(d) { return y(d.week_max); })
-//            .attr("width", x.rangeBand())
-//            .attr("height", function(d) { return height - y(d.week_max); });
+var CNTOCharts = CNTOCharts || {};
+
+CNTOCharts.Summary = {};
+
+CNTOCharts.Summary.setupChart = function(data) {
     var totalWidth = 1200;
     var totalHeight = 500;
 
@@ -149,9 +115,9 @@ function generateChart(data) {
         .attr("dy", "1em")
         .style("text-anchor", "middle")
         .text("Attendance");
-}
+};
 
 $(document).ready(function () {
     var data = {{ event_data|safe }};
-    generateChart(data);
+    CNTOCharts.Summary.setupChart(data);
 });
