0dcd5d5998d53a095a5fc904410772abd7d81529
Riaan Swart <riaan@stonethree.com>
2015-10-27 11:24:35 +0200

    Add and update warnings script runnable.

--- wsgi/cnto/scripts/add_and_update_warnings.py
+++ wsgi/cnto/scripts/add_and_update_warnings.py
@@ -1,3 +1,17 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+## GETTING-STARTED: make sure the next line points to your settings.py:
+os.environ['DJANGO_SETTINGS_MODULE'] = 'cnto.settings'
+## GETTING-STARTED: make sure the next line points to your django project dir:
+if 'OPENSHIFT_REPO_DIR' in os.environ:
+    sys.path.append(os.path.join(os.environ['OPENSHIFT_REPO_DIR'], 'wsgi', 'cnto'))
+
+    from distutils.sysconfig import get_python_lib
+    os.environ['PYTHON_EGG_CACHE'] = get_python_lib()
+
 import django
 
 from datetime import datetime
