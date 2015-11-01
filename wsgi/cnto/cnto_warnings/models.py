0d0618619b48e439bb68adda1037e0511107959f
Riaan Swart <riaan@stonethree.com>
2015-11-01 22:08:19 +0200

    Absence emails for #35.

--- wsgi/cnto/cnto_warnings/models.py
+++ wsgi/cnto/cnto_warnings/models.py
@@ -11,7 +11,7 @@ def recipients_to_recipient_string(recipient_users):
     """
     recipient_emails = []
     for recipient in recipient_users:
-        if len(recipient.email) > 0:
+        if recipient.email is not None and len(recipient.email) > 0:
             recipient_emails.append(recipient.email)
 
     if len(recipient_emails) == 0:
@@ -21,10 +21,22 @@ def recipients_to_recipient_string(recipient_users):
 
 
 class MemberWarningType(models.Model):
+    """
+
+    """
     name = models.TextField(null=False)
+
+    def is_warning(self, warning_name):
+        if warning_name.lower() in self.name.lower():
+            return True
+        else:
+            return False
 
 
 class MemberWarning(CreatedModifiedMixin):
+    """
+
+    """
     member = models.ForeignKey(Member, null=False)
     warning_type = models.ForeignKey(MemberWarningType, null=False)
     message = models.TextField(null=False)
@@ -36,23 +48,27 @@ class MemberWarning(CreatedModifiedMixin):
 
         :return:
         """
-        mod_warning_type = MemberWarningType.objects.get(name__iexact="Mod Assessment Due")
-        grunt_warning_type = MemberWarningType.objects.get(name__iexact="Grunt Qualification Due")
-        contribution_expiring_type = MemberWarningType.objects.get(name__iexact="Contribution Expiring")
-
         recipient_users = []
-        if self.warning_type in [mod_warning_type, grunt_warning_type]:
+        if self.warning_type.is_warning("Mod Assessment Due") or self.warning_type.is_warning(
+                "Grunt Qualification Due"):
             recipient_users = [
                 User.objects.get(username__iexact="admin"),
                 User.objects.get(username__iexact="abuk"),
                 User.objects.get(username__iexact="john"),
             ]
-        elif self.warning_type == contribution_expiring_type:
+        elif self.warning_type.is_warning("Contribution Expiring"):
             recipient_users = [
                 User.objects.get(username__iexact="admin"),
                 User.objects.get(username__iexact="clarke"),
                 User.objects.get(username__iexact="ryujin"),
             ]
+        elif self.warning_type.is_warning("Low Attendance"):
+            recipient_users = [
+                User.objects.get(username__iexact="admin"),
+            ]
+
+            if self.member.member_group.leader is not None:
+                recipient_users.append(self.member.member_group.leader)
 
         if len(recipient_users) > 0:
             return recipients_to_recipient_string(recipient_users)
@@ -64,22 +80,21 @@ class MemberWarning(CreatedModifiedMixin):
 
         :return:
         """
-        mod_warning_type = MemberWarningType.objects.get(name__iexact="Mod Assessment Due")
-        grunt_warning_type = MemberWarningType.objects.get(name__iexact="Grunt Qualification Due")
-        contribution_expiring_type = MemberWarningType.objects.get(name__iexact="Contribution Expiring")
-
         subject = None
         body = None
 
-        if self.warning_type == mod_warning_type:
+        if self.warning_type.is_warning("Mod Assessment Due"):
             subject = "Mod assessment for %s overdue" % (self.member.name,)
             body = "%s didn't report to have his mods assessed within two weeks of joining our community." % (
                 self.member.name,)
-        elif self.warning_type == grunt_warning_type:
+        elif self.warning_type.is_warning("Grunt Qualification Due"):
             subject = "Grunt Qualification for %s overdue" % (self.member.name,)
             body = "%s didn't qualify to become a Grunt within 6 weeks of joining our community." % (self.member.name,)
-        elif self.warning_type == contribution_expiring_type:
+        elif self.warning_type.is_warning("Contribution Expiring"):
             subject = "Contribution for %s expiring" % (self.member.name,)
+            body = "%s" % (self.message,)
+        elif self.warning_type.is_warning("Low Attendance"):
+            subject = "Low Attendance for %s" % (self.member.name,)
             body = "%s" % (self.message,)
 
         return subject, body
