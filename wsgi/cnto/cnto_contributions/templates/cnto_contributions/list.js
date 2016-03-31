febd4fe46f652731f11be4a4f65dba8ea3bb96ea
Riaan Swart <riaan@stonethree.com>
2016-03-31 23:21:59 +0200

    Contribution deletion fixed #55.

--- wsgi/cnto/cnto_contributions/templates/cnto_contributions/list.js
+++ wsgi/cnto/cnto_contributions/templates/cnto_contributions/list.js
@@ -3,7 +3,7 @@ $('.delete-contribution').on('click', function (e) {
     var groupElement = $(this);
     bootbox.confirm("Are you sure you wish to delete this contribution?", function (result) {
         if (result) {
-            var deleteUrl = "/delete-contribution/" + CNTOUtils.getPKFromElement(groupElement).toString();
+            var deleteUrl = "{% url 'delete-contribution' 1 %}".replace(1, CNTOUtils.getPKFromElement(groupElement).toString());
             $('#deleting-contribution-modal').modal('show');
             $.get(deleteUrl, function (data) {
                 window.location.reload();
