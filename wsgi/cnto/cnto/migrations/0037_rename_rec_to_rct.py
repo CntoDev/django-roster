08289ffa15576accca54e1de6c109d32f683ec2a
Riaan Swart <riaan@stonethree.com>
2016-04-29 16:37:05 +0200

    Renamed Rec to Rct.  Fixed #58.

---
+++ wsgi/cnto/cnto/migrations/0037_rename_rec_to_rct.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+def rename_rec_to_rct(apps, schema_editor):
+
+    Rank = apps.get_model("cnto", "Rank")
+
+    rank = Rank.objects.get(name__iexact="res")
+    rank.name = "Rct"
+    rank.save()
+
+
+def rename_rct_to_rec(apps, schema_editor):
+
+    Rank = apps.get_model("cnto", "Rank")
+
+    rank = Rank.objects.get(name__iexact="rct")
+    rank.name = "Rec"
+    rank.save()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('cnto', '0036_no_groups_for_discharged'),
+    ]
+
+    operations = [
+        migrations.RunPython(rename_rec_to_rct, rename_rct_to_rec),
+    ]
