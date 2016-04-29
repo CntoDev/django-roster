6e7ce732012a9bba88e74ce9bef0bf0ed81c34f9
Riaan Swart <riaan@stonethree.com>
2016-04-29 16:56:37 +0200

    Renaming ranks.  Fixed #58.

---
+++ wsgi/cnto/cnto/migrations/0038_rename_rec_to_rct_take2.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+def rename_rec_to_rct(apps, schema_editor):
+
+    Rank = apps.get_model("cnto", "Rank")
+
+    rank = Rank.objects.get(name__iexact="rct")
+    rank.name = "Res"
+    rank.save()
+
+    rank = Rank.objects.get(name__iexact="rec")
+    rank.name = "Rct"
+    rank.save()
+
+
+def rename_rct_to_rec(apps, schema_editor):
+
+    Rank = apps.get_model("cnto", "Rank")
+
+    rank = Rank.objects.get(name__iexact="res")
+    rank.name = "Rct"
+    rank.save()
+
+    rank = Rank.objects.get(name__iexact="rct")
+    rank.name = "Rec"
+    rank.save()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('cnto', '0037_rename_rec_to_rct'),
+    ]
+
+    operations = [
+        migrations.RunPython(rename_rec_to_rct, rename_rct_to_rec),
+    ]
